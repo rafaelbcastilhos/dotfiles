a41e36d9aba93d0d0b0468a3aa2383f58b48eb3e
Rafael B. Castilhos <rafaelbcastilhos@gmail.com>
2020-12-03 22:56:37 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,9 +13,6 @@ sudo apt-get install google-chrome-stable
 echo "installing vscode..."
 sudo apt-get install code
 
-echo "installing git..."
-sudo apt-get install git
-
 echo "installing spotify..."
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90 2EBF997C15BDA244B6EBF5D84773BD5E130D1D45
 sudo apt-get install spotify-client
